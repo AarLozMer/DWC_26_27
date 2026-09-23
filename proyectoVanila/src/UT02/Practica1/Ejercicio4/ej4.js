2cfbd27e079f14a6489e222715a6326b29ddd1ee
aarlozmer <aarlozmer@alu.edu.gva.es>
2026-09-23 19:19:20 +0200

    Cambios en actividades

--- proyectoVanila/src/UT02/Practica1/Ejercicio4/ej4.js
+++ proyectoVanila/src/UT02/Practica1/Ejercicio4/ej4.js
@@ -1,5 +1,25 @@
 "use strict";
 
-function comprobarProducto(nombre, precio, impuesto) {
-    
+function comprobarProducto(nombre="Producto genérico", precio=100, impuesto=21) {
+    try {
+        if (isNaN(precio)) {
+            throw new Error("No es un numero");
+        }
+        else if (isNaN(impuesto)) {
+            throw new Error("La cantidad de impuestos aplicada no es un numero");
+        }
+        else if (precio < 0) {
+            throw new Error("El precio es menor que 0, introduzcalo de nuevo");
+        }
+        else if (impuesto < 0 || impuesto > 100) {
+            throw new Error("Los impuestos atribuidos al producto deben estar entre 0 y 100");
+        }
+        else {
+            return nombre, precio, impuesto;
+        }
+    } catch (Error) {
+        console.error(Error.message);
+    }
 }
+
+export {comprobarProducto};
--- proyectoVanila/src/UT02/Practica1/Ejercicio4/main.js
+++ proyectoVanila/src/UT02/Practica1/Ejercicio4/main.js
@@ -1,1 +1,7 @@
 "use strict";
+
+import {comprobarProducto} from "./ej4.js";
+
+console.log(comprobarProducto("Esparragos", 12, 23));
+console.log(comprobarProducto("", -4, 45));
+console.log(comprobarProducto("", 30, 140));
