2291580b608a9530f42fec3641376c0ae0aa0739
aarlozmer <aarlozmer@alu.edu.gva.es>
2026-09-23 17:41:24 +0200

    Cambios en actividades

--- proyectoVanila/src/UT02/Practica1/Ejercicio3/ej3.js
+++ proyectoVanila/src/UT02/Practica1/Ejercicio3/ej3.js
@@ -1,12 +1,13 @@
 "use strict";
 
 function iteracionNumeros(numero1, numero2) {
-    let total = numero2;
-    let array = new Array[numero1];
-    array[0] = numero2;
+    let final = "";
+    let multiplo = numero2;
     for (let i = 1; i < numero1; i++) {
-        array[i]
+        final+= multiplo + " ";
+        multiplo*=2;
     }
+    return final;
 }
 
 export {iteracionNumeros};
--- proyectoVanila/src/UT02/Practica1/Ejercicio3/main.js
+++ proyectoVanila/src/UT02/Practica1/Ejercicio3/main.js
@@ -1,5 +1,5 @@
 "use strict";
 
-import { iteracionNumeros } from "./ej3.js";
+import  {iteracionNumeros} from "./ej3.js";
 
 console.log(iteracionNumeros(10,5));
